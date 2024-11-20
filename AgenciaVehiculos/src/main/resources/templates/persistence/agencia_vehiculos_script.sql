c36485016b9e35384bc79f2fd250bd2d83b05db5
Matias <matiasfarach@gmail.com>
2024-11-20 09:51:45 -0300

    Módulo de persistencia

---
+++ AgenciaVehiculos/src/main/resources/templates/persistence/agencia_vehiculos_script.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- Schema agencia_vehiculos_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `agencia_vehiculos_db` DEFAULT CHARACTER SET utf8 ;
+USE `agencia_vehiculos_db` ;
